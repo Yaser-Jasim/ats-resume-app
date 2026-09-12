d4b175c4d56c2a8df85ae6a8752ef342cda7cde4
Yaser Jasim <yaser.printings@gmail.com>
2026-09-11 19:35:08 -0700

    updated app/page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,27 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClient } from '@/lib/supabaseClient'
 import { LanguageProvider } from '@/components/marketing/LanguageContext'
 import WelcomeContent from './welcome/page'
 
 export default function HomePage() {
+  const [checking, setChecking] = useState(true)
+  const router = useRouter()
+  const supabase = createClient()
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      if (data.user) {
+        router.replace('/app')
+      } else {
+        setChecking(false)
+      }
+    })
+  }, [])
+
+  if (checking) return null
+
   return (
     <LanguageProvider>
       <WelcomeContent />
